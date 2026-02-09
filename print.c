00be768fc9ce1eb5fb3b61377367ddab0b76b89a
Samar <samarjomysujith606@gmail.com>
2026-02-09 20:21:21 +0530

    working with hex to string

--- print.c
+++ print.c
@@ -10,6 +10,28 @@ static int read_string(char *buffer,int position, const char *string) {
     return index; //returns the number of characters being copied
 }
 
+static int hex_to_string(char *buffer, int position, uint64_t digits) {
+    char digits_buffer[25]; //to save the converted digits
+    char digit_map[16]="0123456789ABCDEF";
+    int size=0;
+
+    while (digits!=0)
+    {
+        digits_buffer[size++]=digit_map[digits%16];
+        digits/=16;
+
+    }
+    while (int i=size-1;i>=0;i--)
+    {
+        buffer[position++]=digits_buffer[i]; //since we converteed ts from right to left
+    }
+    buffer[position++]='x';
+    buffer[position++]='0';
+        
+    return size+2;
+
+}
+
 int printk(const char *format, ...) {
     char buffer[1024]; //ensure that the value printed is not larger than thisx
     int buffer_size=0;
@@ -25,7 +47,8 @@ int printk(const char *format, ...) {
         }
         else {
             switch (format[++i]) {
-                case 'x':
+                case 'x': //hexadecimal value
+                    buffer_size+= hex_to_string(buffer,buffer_size,(uint64_t)integer)
                     break;
                 case 'u':
                     break;
